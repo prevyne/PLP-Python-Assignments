d92e39a59b264e2d571fcf2e7af5b5907e86aa96
prevyne <prevyneoketch6@gmail.com>
2025-04-09 03:34:50 +0300

    update

--- BasicCalc.py
+++ BasicCalc.py
@@ -5,13 +5,15 @@ num2=int(input("Enter the second number: "))
 operator=str(input("Enter the operator (+, -, *, /): "))
 result=0
 
-if operator=='+':
-    result=num1+num2
-elif operator=='-':
-    result=num-num2
-elif operator=='*':
-    result=num1*num2
-elif operator=='/':
-    result=num1/num2
+def simple_math(num1,num2,operator,result):
+    if operator=='+':
+        result=num1+num2
+    elif operator=='-':
+        result=num-num2
+    elif operator=='*':
+        result=num1*num2
+    elif operator=='/':
+        result=num1/num2
+    print("{} {} {} = {}".format(num1,operator,num2,result))
 
-print("{} {} {} = {}".format(num1,operator,num2,result))
+simple_math(num1,num2,operator,result)
